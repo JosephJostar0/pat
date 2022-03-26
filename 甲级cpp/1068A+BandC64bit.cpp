742c6a344c687390282b6d368a1c117ee17e06b7
JosephJostar0 <bohanxiong56@gmail.com>
2022-03-26 18:20:58 +0800

    重墙绕院更重门，春风无路通深意

---
+++ 甲级cpp/1068A+BandC64bit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int       T, i;
+    long long a, b, c, d;
+    scanf("%d", &T);
+    for (i = 1; i <= T; i++) {
+        scanf("%lld%lld%lld", &a, &b, &c);
+        d = a + b;
+        if (a > 0 && b > 0 && d < 0)
+            printf("Case #%d: true\n", i);
+        else if (a < 0 && b < 0 && d >= 0)
+            printf("Case #%d: false\n", i);
+        else if (d > c)
+            printf("Case #%d: true\n", i);
+        else
+            printf("Case #%d: false\n", i);
+    }
+    return 0;
+}
